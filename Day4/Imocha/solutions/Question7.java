1efe572ce93f922fd062ac54b490da3679702152
Tony Hano <tony.hano@gmail.com>
2025-06-15 02:40:46 +0300

    Solution Question7.java
    hellooooo
    
    https://github.com/Tony-Hano

--- Day4/Imocha/solutions/Question7.java
+++ Day4/Imocha/solutions/Question7.java
@@ -1,1 +1,28 @@
+// Hi it is me Tony Hano https://github.com/Tony-Hano
+import java.util.*;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            arr1[i] = x;
+        }
+
+        for (int l = 0; l < arr1.length - 1; l++) {
+            int counter = 0;
+            for (int j = l + 1; j < arr1.length; j++) {
+                if (arr1[l] > arr1[j]) {
+                    counter++;
+                }
+            }
+            System.out.println(counter);
+        }
+        System.out.println("0");
+
+    }
+}
